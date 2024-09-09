0ea3ecd6e4d0d940b34298fadb53b6b3b2f80fcc
Breno <brenospsb@gmail.com>
2024-09-09 16:05:32 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let nomeHeroi = "Antedeguemon"
+let xp = 427
+if(xp <= 1000){console.log("O herói de nome " + nomeHeroi + " está no ranking Ferro")}
+else if(xp <= 2000){console.log("O herói de nome " + nomeHeroi + " está no ranking Bronze")}
+else if(xp <= 5000){console.log("O herói de nome " + nomeHeroi + " está no ranking Prata")}
+else if(xp <= 7000){console.log("O herói de nome " + nomeHeroi + " está no ranking Ouro")}
+else if(xp <= 8000){console.log("O herói de nome " + nomeHeroi + " está no ranking Platina")}
+else if(xp <= 9000){console.log("O herói de nome " + nomeHeroi + " está no ranking Ascendente")}
+else if(xp <= 10000){console.log("O herói de nome " + nomeHeroi + " está no ranking Imortal")}
+else if(xp >= 10001){console.log("O herói de nome " + nomeHeroi + " está no ranking Radiante")}
